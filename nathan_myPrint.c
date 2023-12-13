22dfb101794609cc2b5b6e951e9c91daa08796d4
NathanBaraka <nathanbaraka23@gmail.com>
2023-12-13 01:27:03 -0800

    simple shell

--- nathan_myPrint.c
+++ nathan_myPrint.c
@@ -1,8 +1,10 @@
 #include "shell.h"
+/*
+ * This is my own printf function
+ */
 
-
-void nathan_myPrint(const char *myown_format) {
-//my unique message
+void nathan_myPrint(const char *myown_format) 
+{
    write(STDOUT_FILENO, myown_format, strlen(myown_format));
 }
 
