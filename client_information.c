deef76d435e7840b42f68069dc70472a0e4749d1
NathanBaraka <nathanbaraka23@gmail.com>
2023-12-13 06:25:53 -0800

    simple_shell

--- client_information.c
+++ client_information.c
@@ -1,29 +1,28 @@
 #include "shell.h"
 
 /**
- * client_rule - Receive string input from the user
+ * client_information - Receive string input from the user
  * @rule: Buffer to store user input
  * @size: Size of the buffer
  *
  * This function receives string input from the user. If there is no input,
  * then just a new line is printed; otherwise, an error message is printed.
  */
-void client_rule(char *rule, size_t size)
+void client_information(char *rule, size_t size)
 {
-    if (fgets(rule, size, stdin) == NULL)
-    {
-        if (feof(stdin))
-        {
-            nathan_myPrint("\n");
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            perror("Error occurred while reading input");
-            exit(EXIT_FAILURE);
-        }
-    }
-    rule[strcspn(rule, "\n")] = '\0';
-    /* Removes a newline. */
+	if (fgets(rule, size, stdin) == NULL)
+	{
+		if (feof(stdin))
+		{
+			nathan_myPrint("\n");
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("Error occurred while reading input");
+			exit(EXIT_FAILURE);
+		}
+	}
+	rule[strcspn(rule, "\n")] = '\0'; /* Removes a newline. */
 }
 
