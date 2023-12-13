cc58112257f2234f3983eb93944e254ad0995471
NathanBaraka <nathanbaraka23@gmail.com>
2023-12-13 08:01:22 -0800

    simple_shell

--- rule.c
+++ rule.c
@@ -14,24 +14,21 @@
  */
 void enact_rule(const char *rule)
 {
-    pid_t child_pid = fork(); 
+pid_t child_pid = fork();
 
-    if (child_pid == -1)
-    {
-        perror("Fork");
-        exit(EXIT_FAILURE);
-    }
-    else if (child_pid == 0)
-    {
-
-        execlp(rule, rule, (char *)NULL);
-        perror("execlp error");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-
-        wait(NULL);
-    }
+if (child_pid == -1)
+{
+perror("Fork");
+exit(EXIT_FAILURE);
 }
-
+else if (child_pid == 0)
+{
+execlp(rule, rule, (char *)NULL);
+perror("execlp error");
+exit(EXIT_FAILURE);
+}
+else
+{
+wait(NULL);
+}
+}
