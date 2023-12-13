b6491b0a06597b5a74ce977f77f202479b429962
NathanBaraka <nathanbaraka23@gmail.com>
2023-12-13 14:26:49 -0800

    simple_shell

--- shell.c
+++ shell.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include "path_utils.h"
 
 /**
  * main - Entry point for the shell program
---
+++ utils.h
@@ -0,0 +1,6 @@
+#ifndef PATH_UTILS_H
+#define PATH_UTILS_H
+
+char *construct_full_path(const char *command);
+
+#endif /* PATH_UTILS_H */
