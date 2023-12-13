db5adaade20f67faa9329dfd6fe97cf809759d30
NathanBaraka <nathanbaraka23@gmail.com>
2023-12-13 01:35:22 -0800

    simple shell

--- show_prompt.c
+++ show_prompt.c
@@ -1,6 +1,8 @@
 #include "shell.h"
  
-//This is the function to display the new prompt.
+/*This is the function to display the new prompt
+ * on the terminal
+ */
  void show_prompt(void) {
     nathan_myPrint("Nathan_shell$$ ");
 }
