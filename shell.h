2271d38b25777a6c1a27974cee7126ef99d3f594
NathanBaraka <nathanbaraka23@gmail.com>
2023-12-13 01:33:55 -0800

    simple shell

--- shell.h
+++ shell.h
@@ -1,12 +1,16 @@
 #ifndef SHELL_H
 #define SHELL_H
-
+/* 
+ * These are the the libraries to be used.
+ */
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
+/*
+ * these are the prototypes on each program.
+ */
 void show_prompt(void);
 void nathan_myPrint(const char *myown_format);
 void enact_rule(const char *rule);
