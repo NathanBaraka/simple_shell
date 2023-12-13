c97ba3a403f87974f68e22afc7dc46a117504d01
NathanBaraka <nathanbaraka23@gmail.com>
2023-12-13 01:30:12 -0800

    simple shell

--- rule.c
+++ rule.c
@@ -2,19 +2,25 @@
 
  void enact_rule(const char *rule) {
      pid_t child_pid = fork(); 
-//This function creates a child process
+/*
+ * This function creates a child process
+ * using folk
+ */
 
    if (child_pid == -1) {
         perror("Part");
          exit(EXIT_FAILURE);
 
      } else if (child_pid == 0) {
-// This is the Child process.
+/*This is the Child process that has been created
+ */
         execlp(rule, rule, (char *)NULL);
         perror("execlp error");
         exit(EXIT_FAILURE);
      } else {
-// This is the Parent process that waits until the child process is done.
+/*This is the Parent process that uses the wait function.
+ * Until the child process is done.
+ */
          wait(NULL);
     }
  }
